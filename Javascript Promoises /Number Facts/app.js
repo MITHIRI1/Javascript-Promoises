361fcfeafc7b41de1f301f5c0966d4ec79900fa4
MITHIRI1 <87658049+MITHIRI1@users.noreply.github.com>
2022-06-04 14:24:45 -0400

    Add files via upload

---
+++ Javascript Promoises /Number Facts/app.js
@@ -0,0 +1,39 @@
+// Part 1: Number Facts
+
+let url = 'http://numbersapi.com/';
+
+// 1.
+let faveNumPromise = axios.get(`${url}7?json`);
+faveNumPromise.then((res) => console.log(res.data.text));
+
+
+// 2.
+
+let multipleNumsPromise = [];
+for (let i = 1; i < 5; i++) {
+  multipleNumsPromise.push(axios.get(`${url}${i}?json`));
+}
+
+Promise.all(multipleNumsPromise).then((multipleNumsPromise) => {
+  for (res of multipleNumsPromise) {
+    console.log(res.data.text);
+  }
+});
+
+
+let multNums = [55, 2, 3];
+$.getJSON(`${url}${multNums}?json`).then((data) => {
+  //   console.log(data);
+});
+
+
+// 3.
+
+// The way that is shown in the solution, still displays undefined
+Promise.all(
+  Array.from({ length: 4 }, () => {
+    return $.getJSON(`${baseURL}/${favNumber}?json`);
+  })
+).then((facts) => {
+  facts.forEach((data) => $('body').append(`<p>${data.text}</p>`));
+});
